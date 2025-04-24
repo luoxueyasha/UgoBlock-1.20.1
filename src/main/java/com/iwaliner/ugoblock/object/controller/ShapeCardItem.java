cd2f5c39b637340af4c00ce976e2e1315160d124
luoxueyasha <bing1273103582@outlook.com>
2025-04-24 23:37:59 +0800

    update performance by about 1500%+, debug on MovingBlockEntity#makeCollisionEntity and ShapeCardItem#useOn. Not sure if works perfectly correct, so just a code backup

--- src/main/java/com/iwaliner/ugoblock/object/controller/ShapeCardItem.java
+++ src/main/java/com/iwaliner/ugoblock/object/controller/ShapeCardItem.java
@@ -21,122 +21,191 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class ShapeCardItem extends Item {
     public ShapeCardItem(Properties p_41383_) {
         super(p_41383_);
     }
+
     @Override
     public InteractionResult useOn(UseOnContext context) {
-        Level level=context.getLevel();
-        BlockPos pos=context.getClickedPos();
-        ItemStack stack=context.getItemInHand();
-        List<BlockPos> list=new ArrayList<>();
-        Player player=context.getPlayer();
-        int size=stack.getCount();
-        boolean flag=false;
-        if(stack.getItem() instanceof ShapeCardItem&&player!=null) {
-                CompoundTag tag = stack.getTag();
-                if (tag == null) {
-                    tag = new CompoundTag();
+        Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        ItemStack stack = context.getItemInHand();
+        List<BlockPos> list = new ArrayList<>();
+        Player player = context.getPlayer();
+        int size = stack.getCount();
+        boolean flag = false;
+        if (stack.getItem() instanceof ShapeCardItem && player != null) {
+            CompoundTag tag = stack.getTag();
+            if (tag == null) {
+                tag = new CompoundTag();
+            }
+            if (!tag.contains("positionList")) {
+                tag.put("positionList", new CompoundTag());
+            }
+            CompoundTag posTag = tag.getCompound("positionList");
+            CompoundTag backup = posTag.copy();
+            /**このタグは、tureのときfalseにした上で範囲選択終了処理を行い、falseのときはtrueにしたうえで範囲選択開始処理を行う*/
+            // @debug starts
+
+            HashSet<BlockPos> existingPositions = new HashSet<>();
+            int currentSize = 0;
+
+            // Travel through current positions and first index only once
+            int firstAvailableIndex = -1;
+            for (int i = 0; i < Utils.getMaxSize(); i++) {
+                String key = "location_" + i;
+                if (posTag.contains(key)) {
+                    BlockPos existingPos = NbtUtils.readBlockPos(posTag.getCompound(key));
+                    existingPositions.add(existingPos);
+                    currentSize++;
+                } else if (firstAvailableIndex == -1) {
+                    firstAvailableIndex = i;
                 }
-                if (!tag.contains("positionList")) {
-                    tag.put("positionList", new CompoundTag());
-                }
-                CompoundTag posTag = tag.getCompound("positionList");
-                CompoundTag backup = posTag.copy();
-                /**このタグは、tureのときfalseにした上で範囲選択終了処理を行い、falseのときはtrueにしたうえで範囲選択開始処理を行う*/
+            }
+
+            if (firstAvailableIndex != -1) {
                 if (!tag.contains("select")) {
                     tag.putBoolean("select", false);
                 }
-                tag.putBoolean("select", !tag.getBoolean("select"));
+                boolean isSelecting = !tag.getBoolean("select");
+                tag.putBoolean("select", isSelecting);
 
-                int ii = -1;
-                for (int i = 0; i < Utils.getMaxSize(); i++) {
-                    if (!posTag.contains("location_" + String.valueOf(i))) {
-                        ii = i;
-                        break;
-                    } else {
-                        list.add(NbtUtils.readBlockPos(posTag.getCompound("location_" + String.valueOf(i))));
-                    }
-                }
-                if (ii != -1) {
-                    if (tag.getBoolean("select")) {
-                        /**範囲選択の始点を登録*/
-                        tag.put("edge_A", NbtUtils.writeBlockPos(pos));
-                    } else {
-                        /**範囲選択の終点は今クリックした地点なので、始点も呼び出すことで範囲が確定*/
-                        BlockPos edgeA = NbtUtils.readBlockPos(tag.getCompound("edge_A"));
-                        for (int i = 0; i <= Math.abs(edgeA.getX() - pos.getX()); i++) {
-                            for (int j = 0; j <= Math.abs(edgeA.getY() - pos.getY()); j++) {
-                                for (int k = 0; k <= Math.abs(edgeA.getZ() - pos.getZ()); k++) {
-                                    BlockPos pos2 = pos.offset(edgeA.getX() - pos.getX() >= 0 ? i : -i, edgeA.getY() - pos.getY() >= 0 ? j : -j, edgeA.getZ() - pos.getZ() >= 0 ? k : -k);
-                                    int ii2 = -1;
-                                    for (int i0 = 0; i0 < Utils.getMaxSize(); i0++) {
-                                        if (!posTag.contains("location_" + String.valueOf(i0))) {
-                                            ii2 = i0;
-                                            break;
-                                        }
-                                    }
-                                    if (ii2 != -1 && !list.contains(pos2)) {
-                                        if ((player.isSuppressingBounce()) || !level.getBlockState(pos2).isAir()) {
-                                            posTag.put("location_" + String.valueOf(ii2), NbtUtils.writeBlockPos(pos2));
-                                        }
+                if (isSelecting) {
+                    tag.put("edge_A", NbtUtils.writeBlockPos(pos));
+                } else {
+                    BlockPos edgeA = NbtUtils.readBlockPos(tag.getCompound("edge_A"));
+
+                    // pre-calculate range
+                    int xDiff = Math.abs(edgeA.getX() - pos.getX());
+                    int yDiff = Math.abs(edgeA.getY() - pos.getY());
+                    int zDiff = Math.abs(edgeA.getZ() - pos.getZ());
+                    int xDir = edgeA.getX() - pos.getX() >= 0 ? 1 : -1;
+                    int yDir = edgeA.getY() - pos.getY() >= 0 ? 1 : -1;
+                    int zDir = edgeA.getZ() - pos.getZ() >= 0 ? 1 : -1;
+
+                    // use single index counter
+                    int newPosIndex = firstAvailableIndex;
+
+                    // add new index
+                    for (int i = 0; i <= xDiff && newPosIndex < Utils.getMaxSize(); i++) {
+                        for (int j = 0; j <= yDiff && newPosIndex < Utils.getMaxSize(); j++) {
+                            for (int k = 0; k <= zDiff && newPosIndex < Utils.getMaxSize(); k++) {
+                                BlockPos newPos = pos.offset(i * xDir, j * yDir, k * zDir);
+
+                                if (!existingPositions.contains(newPos)) {
+                                    if (player.isSuppressingBounce() || !level.getBlockState(newPos).isAir()) {
+                                        posTag.put("location_" + newPosIndex, NbtUtils.writeBlockPos(newPos));
+                                        existingPositions.add(newPos);
+                                        newPosIndex++;
                                     }
                                 }
                             }
                         }
+                    }
 
-                        /**始点をリセット*/
-                        tag.put("edge_A", NbtUtils.writeBlockPos(Utils.errorPos()));
-                    }
-                    flag=true;
+                    tag.put("edge_A", NbtUtils.writeBlockPos(Utils.errorPos()));
                 }
-                if (posTag.size() >= Utils.getMaxSize()) {
-                    if (context.getPlayer() != null && !level.isClientSide) {
-                        context.getPlayer().displayClientMessage(Component.translatable("info.ugoblock.reachedPositionSelectLimit", Utils.getMaxSize() - 1).withStyle(ChatFormatting.RED), false);
-                    }
-                    CompoundTag tag1 = new CompoundTag();
-                    tag1.put("edge_A", NbtUtils.writeBlockPos(Utils.errorPos()));
-                    tag1.put("positionList", backup);
-                    stack.setTag(tag1);
-                } else {
-                    stack.setTag(tag);
+                flag = true;
+            }
+            // if (!tag.contains("select")) {
+            //     tag.putBoolean("select", false);
+            // }
+            // tag.putBoolean("select", !tag.getBoolean("select"));
+
+            // int ii = -1;
+            // for (int i = 0; i < Utils.getMaxSize(); i++) {
+            //     if (!posTag.contains("location_" + String.valueOf(i))) {
+            //         ii = i;
+            //         break;
+            //     } else {
+            //         list.add(NbtUtils.readBlockPos(posTag.getCompound("location_" + String.valueOf(i))));
+            //     }
+            // }
+            // if (ii != -1) {
+            //     if (tag.getBoolean("select")) {
+            //         /**範囲選択の始点を登録*/
+            //         tag.put("edge_A", NbtUtils.writeBlockPos(pos));
+            //     } else {
+            //         /**範囲選択の終点は今クリックした地点なので、始点も呼び出すことで範囲が確定*/
+            //         BlockPos edgeA = NbtUtils.readBlockPos(tag.getCompound("edge_A"));
+            //         for (int i = 0; i <= Math.abs(edgeA.getX() - pos.getX()); i++) {
+            //             for (int j = 0; j <= Math.abs(edgeA.getY() - pos.getY()); j++) {
+            //                 for (int k = 0; k <= Math.abs(edgeA.getZ() - pos.getZ()); k++) {
+            //                     BlockPos pos2 = pos.offset(edgeA.getX() - pos.getX() >= 0 ? i : -i, edgeA.getY() - pos.getY() >= 0 ? j : -j, edgeA.getZ() - pos.getZ() >= 0 ? k : -k);
+            //                     int ii2 = -1;
+            //                     for (int i0 = 0; i0 < Utils.getMaxSize(); i0++) {
+            //                         if (!posTag.contains("location_" + String.valueOf(i0))) {
+            //                             ii2 = i0;
+            //                             break;
+            //                         }
+            //                     }
+            //                     if (ii2 != -1 && !list.contains(pos2)) {
+            //                         if ((player.isSuppressingBounce()) || !level.getBlockState(pos2).isAir()) {
+            //                             posTag.put("location_" + String.valueOf(ii2), NbtUtils.writeBlockPos(pos2));
+            //                         }
+            //                     }
+            //                 }
+            //             }
+            //         }
+
+            //         /**始点をリセット*/
+            //         tag.put("edge_A", NbtUtils.writeBlockPos(Utils.errorPos()));
+            //     }
+            //     flag=true;
+            // }
+
+            // @debug ends
+
+            if (posTag.size() >= Utils.getMaxSize()) {
+                if (context.getPlayer() != null && !level.isClientSide) {
+                    context.getPlayer().displayClientMessage(Component.translatable("info.ugoblock.reachedPositionSelectLimit", Utils.getMaxSize() - 1).withStyle(ChatFormatting.RED), false);
                 }
-                if (stack.getCount() > 1) {
-                    ItemStack newStack = new ItemStack(Register.shape_card.get(), size - 1);
-                    stack = stack.copyWithCount(1);
-                    if (!player.getInventory().add(newStack)) {
-                        player.drop(newStack, false);
-                    }
+                CompoundTag tag1 = new CompoundTag();
+                tag1.put("edge_A", NbtUtils.writeBlockPos(Utils.errorPos()));
+                tag1.put("positionList", backup);
+                stack.setTag(tag1);
+            } else {
+                stack.setTag(tag);
+            }
+            if (stack.getCount() > 1) {
+                ItemStack newStack = new ItemStack(Register.shape_card.get(), size - 1);
+                stack = stack.copyWithCount(1);
+                if (!player.getInventory().add(newStack)) {
+                    player.drop(newStack, false);
                 }
-                player.setItemInHand(context.getHand(), stack);
-            if(level.getBlockEntity(pos) instanceof AbstractControllerBlockEntity blockEntity&&flag) {
+            }
+            player.setItemInHand(context.getHand(), stack);
+            if (level.getBlockEntity(pos) instanceof AbstractControllerBlockEntity blockEntity && flag) {
                 player.level().playSound(player, pos, SoundEvents.ENDER_CHEST_OPEN, SoundSource.BLOCKS, 1F, 1F);
-            }else{
+            } else {
                 level.playSound(context.getPlayer(), pos, SoundEvents.UI_STONECUTTER_SELECT_RECIPE, SoundSource.BLOCKS, 1F, 1F);
             }
-                return InteractionResult.SUCCESS;
-            }
-       return InteractionResult.FAIL;
+            return InteractionResult.SUCCESS;
+        }
+        return InteractionResult.FAIL;
     }
-   public boolean isFoil(ItemStack stack) {
-        return stack.getTag()!=null&&stack.getTag().getBoolean("select");
+
+    public boolean isFoil(ItemStack stack) {
+        return stack.getTag() != null && stack.getTag().getBoolean("select");
     }
+
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> list, TooltipFlag flag) {
         list.add(Component.translatable("info.ugoblock.shape_card").withStyle(ChatFormatting.GREEN));
-        if(stack.getTag()!=null) {
-            int size=Utils.getPositionList(stack.getTag()).size();
-            for (int i = 0;size>5? i < 5 : i<size; i++) {
+        if (stack.getTag() != null) {
+            int size = Utils.getPositionList(stack.getTag()).size();
+            for (int i = 0; size > 5 ? i < 5 : i < size; i++) {
                 BlockPos pos = NbtUtils.readBlockPos(stack.getTag().getCompound("positionList").getCompound("location_" + String.valueOf(i)));
                 if (!pos.equals(Utils.errorPos())) {
                     list.add(Component.translatable("info.ugoblock.shape_card_location").append("[").append(String.valueOf(pos.getX())).append(", ").append(String.valueOf(pos.getY())).append(", ").append(String.valueOf(pos.getZ())).append("]").withStyle(ChatFormatting.GRAY));
                 }
             }
-            if(size>5) {
-                list.add(Component.translatable("info.ugoblock.shape_card_location_other",size).withStyle(ChatFormatting.GRAY));
+            if (size > 5) {
+                list.add(Component.translatable("info.ugoblock.shape_card_location_other", size).withStyle(ChatFormatting.GRAY));
             }
         }
     }
